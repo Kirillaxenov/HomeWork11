fb3668a3b86e368b9933fa9fdc2977fba8a51b99
Kirill <kaxyonov@gmail.com>
2023-05-08 16:25:42 +0300

    verify

---
+++ src/main/java/org/example/Film.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Film {
+    private int id;
+    private String name;
+    private String imageUrl;
+    private String genre;
+
+    public Film(int id, String name, String imageUrl, String genre) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.genre = genre;
+    }
+
+}
